f3037ae9d36d45f92114d22afdc0b8ec73b67cf6
swagballer <szhao66@illinois.edu>
2024-03-05 11:23:10 -0600

    Updated cursor

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ function showPopup() {
 function closePopup() {
     document.getElementById('popup').style.display = 'none';
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const cursor = document.querySelector(".custom-cursor");
+
+    document.addEventListener("mousemove", function (e) {
+        const { clientX, clientY } = e;
+        cursor.style.left = `${clientX}px`;
+        cursor.style.top = `${clientY}px`;
+    });
+
+    // hover effect
+    const clickableElements = document.querySelectorAll(".clickable");
+
+    clickableElements.forEach((element) => {
+        element.addEventListener("mouseover", () => {
+            cursor.classList.add("hovered");
+        });
+
+        element.addEventListener("mouseout", () => {
+            cursor.classList.remove("hovered");
+        });
+    });
+});
