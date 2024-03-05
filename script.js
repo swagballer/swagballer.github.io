c2f682f1b7b26e533ddc42d3e850b26ecaf17b51
swagballer <szhao66@illinois.edu>
2024-03-05 11:07:29 -0600

    Added popups and icons

---
+++ script.js
@@ -0,0 +1,8 @@
+function showPopup() {
+    document.getElementById('popup').style.display = 'block';
+}
+
+// Function to close the popup
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
